8d18fa01351034e4a1a54fb82eedfce04111d13d
wojab <wojciech.jablonski1987@gmail.com>
2017-04-26 20:28:23 +0200

    Packages have been created for different spheres of responsibility

---
+++ test/test_add_contact.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import pytest
+
+from fixture.application2 import Application2
+from model.contact import Contact
+
+
+@pytest.fixture
+def app(request):
+    fixture = Application2()
+    request.addfinalizer(fixture.destroy())
+    return fixture
+
+def test_test_add_contact(app):
+    app.login(username="admin", password="secret")
+    app.create_contact(Contact(firstname="wojtek", middlename="marek", lastname="wojtasik", nickname="nico",
+                                        title="newone", company="qa", address="Poland", home="-", mobile="-", work="-",
+                                        fax="-",
+                                        email="test@test.pl", email3="-", email2="-", homepage="-", byear="1980",
+                                        ayear="1990", address2="Poland, lodz"))
+    app.logout()
