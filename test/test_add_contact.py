7d3b7b0f87d62b4c39b43fce7358f9588427259f
wojab <wojciech.jablonski1987@gmail.com>
2017-04-26 21:59:10 +0200

    Isolated class for working with groups

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,20 +1,18 @@
-# -*- coding: utf-8 -*-
 import pytest
 from fixture.application2 import Application2
 from model.contact import Contact
 
-
 @pytest.fixture
 def app(request):
     fixture = Application2()
-    request.addfinalizer(fixture.destroy())
+    request.addfinalizer(fixture.destroy)
     return fixture
 
-def test_test_add_contact(app):
-    app.login(username="admin", password="secret")
-    app.create_contact(Contact(firstname="wojtek", middlename="marek", lastname="wojtasik", nickname="nico",
-                                        title="newone", company="qa", address="Poland", home="-", mobile="-", work="-",
-                                        fax="-",
-                                        email="test@test.pl", email3="-", email2="-", homepage="-", byear="1980",
-                                        ayear="1990", address2="Poland, lodz"))
-    app.logout()
+def test_add_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.create(Contact(firstname="wojtek", middlename="marek", lastname="wojtasik", nickname="nico",
+                              title="newone", company="qa", address="Poland", home="-", mobile="-", work="-",
+                              fax="-",
+                              email="test@test.pl", email3="-", email2="-", homepage="-", byear="1980",
+                              ayear="1990", address2="Poland, lodz"))
+    app.session.logout()
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,22 +1,20 @@
 import pytest
-
 from fixture.application import Application
 from model.group import Group
 
-
 @pytest.fixture
 def app(request):
-    fixture=Application()
-    request.addfinalazer(fixture.destroy())
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
     return fixture
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="testowa", header="test", footer="test"))
+    app.group.create(Group(name="testowa", header="test", footer="test"))
     app.session.logout()
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
 
