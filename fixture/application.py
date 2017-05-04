f46c39d995dea08a939baadd446f226df8f67f51
wojab <wojciech.jablonski1987@gmail.com>
2017-05-04 15:10:08 +0200

    Increase «intelligence» of login and logout: check the current state of the session.

--- fixture/application.py
+++ fixture/application.py
@@ -6,7 +6,7 @@ from fixture.contact import ContactHelper
 class Application:
     def __init__(self):
         self.wd = WebDriver()
-        self.wd.implicitly_wait(60)
+        self.wd.implicitly_wait(10)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
