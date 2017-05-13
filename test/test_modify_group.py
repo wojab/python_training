61fa26381f9e13fadd71dbfce507767c121b4ed1
wojab <wojciech.jablonski1987@gmail.com>
2017-05-13 19:06:35 +0200

    Loading the information from the tested application as a list

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -3,15 +3,24 @@ from model.group import Group
 def test_modify_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(name="New group"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 def test_modify_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(name="New header"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 def test_modify_group_footer(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(name="New footer"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
