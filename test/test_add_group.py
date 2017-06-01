95264041b41d9fa326e44622ce7cedb1bd6ae757
wojab <wojciech.jablonski1987@gmail.com>
2017-06-01 15:49:05 +0200

    Parameters from the command line

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,7 +4,7 @@ import random
 import string
 
 def random_string(prefix, maxlen):
-     symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+     symbols = string.ascii_letters + string.digits
      return prefix + "".join([random.choice(symbols) for i in range (random.randrange(maxlen))])
 
 testdata = [Group(name= "", header = "", footer = "" )] + [
