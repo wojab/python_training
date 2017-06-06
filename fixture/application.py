499dbabc291b6324ac6ab94e5f3c61a17a622024
wojab <wojciech.jablonski1987@gmail.com>
2017-06-06 13:12:23 +0200

    fixes (parameters)

--- fixture/application.py
+++ fixture/application.py
@@ -1,25 +1,24 @@
-from selenium import webdriver
+#from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.android.webdriver import WebDriver
+
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
+from selenium import webdriver
 
 class Application:
-    def __init__(self, browser, base_url):
-        if browser == "firefox":
-            self.wd == webdriver.Firefox()
-        elif browser == "chrome":
-            self.wd = webdriver.Chrome()
-        elif browser == "ie":
-            self.wd = webdriver.Ie()
+    def __init__(self, browser="firefox", base_url="http://localhost/addressbook/"):
+        if browser =="firefox":
+           self.wd =webdriver.Firefox()
+
         else:
-            raise ValueError("Unrecognized browser %s" %browser)
+            self.wd= webdriver.Ie()
        # self.wd.implicitly_wait(2)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
         #self.open_home_page()
-        self.base_url = base_url
-
+        self.base_url=base_url
 
     def is_valid(self):
         try:
