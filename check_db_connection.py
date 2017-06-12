baac731c4459010687fbd4bc03aabf125873c39d
wojab <wojciech.jablonski1987@gmail.com>
2017-06-12 16:27:03 +0200

    relations

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,18 +1,10 @@
 from  fixture.orm import ORMFixture
-
+from model.group import Group
 
 db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
-#
-# try:
-#     l = db.get_group_list()
-#     for item in l:
-#         print(item)
-#     print(len(l))
-# finally:
-#     pass
 
 try:
-    l = db.get_contact_list()
+    l = db.get_contacts_in_group(Group(id="69"))
     for item in l:
         print(item)
     print(len(l))
